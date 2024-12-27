1e12cb2e6bbc4b6a072ae21b9bcecc30224eb581
Sumit3972 <sumitmehta396@gmail.com>
2024-12-27 11:46:09 -0800

    remove cart and price functionallity added

--- src/compoents/Cart.jsx
+++ src/compoents/Cart.jsx
@@ -4,6 +4,15 @@ import { CardContext } from '../context/contextApi'
 function Cart() {
 
 
+
+
+
+   function RemoveAllCart(){
+        setCartData([]);
+        localStorage.setItem("cart",JSON.stringify([]));
+   }
+
+
   function Removecart(i){
      let newarr = [...CardData];
      newarr.splice(i,1);
@@ -12,14 +21,25 @@ function Cart() {
   }
 
     const {CardData,setCartData}= useContext(CardContext)
-    console.log(CardData)
+    //console.log(CardData)
+
+    let totalprice = 0;
+    for(let i=0;i<CardData.length;i++){
+      totalprice+= CardData[i].price/100 || CardData[i].defaultPrice /100;     
+    }
+
+
   return (
     <div className='w-full'>
       <div className='w-[50%]  mx-auto'>
             {
               CardData.map((data,i)=>(
-                <div className='flex justify-between'>
-                  <h2>{data.name}</h2>
+                <div className='flex justify-between my-5 p-2'>
+                  <div className='w-[70%]'>
+                  <h2 className='text-3xl'>{data.name}</h2>
+                  <p>₹{data.price/100  ||  data.defaultPrice/100}</p>
+                    </div>
+                 
                   <div className="w-[20%] relative h-full">
           <img
             className="object-cover rounded-[12px] w-[156px] h-[144px]"
@@ -33,6 +53,8 @@ function Cart() {
                   </div>
               ))
             }
+            <h1>Total ₹{totalprice}</h1>
+            <button onClick={RemoveAllCart} className='bg-green-400 my-7  p-3 rounded-lg'>Clear Cart</button>
       </div>
     </div>
   )
